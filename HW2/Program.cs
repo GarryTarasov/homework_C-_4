7efa7f52f73c3ebaa19b45825f24275f1470c15f
Garry <igor_tarasov_84@mail.ru>
2022-08-05 13:30:40 +0500

    end homework4 task 2

---
+++ HW2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Ввелите число");
+int nunb = int.Parse(Console.ReadLine());
+int summ = 0;
+while (nunb > 0)
+{
+summ = summ + nunb % 10;
+nunb = nunb /10 ;
+ }
+ Console.WriteLine(summ);
